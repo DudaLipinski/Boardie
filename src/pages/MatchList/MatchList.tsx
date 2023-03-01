9ecf4c25c73d88d5e716c08d1634f063f256aa02
MariaLipinski <dudalipinski@hotmail.com>
2023-03-01 16:26:12 +0100

    feat(matchList): develop button to nagivate to create new match

--- src/pages/MatchList/MatchList.tsx
+++ src/pages/MatchList/MatchList.tsx
@@ -3,10 +3,12 @@ import { useOwnMatches } from '../../hooks/useOwnMatches'
 
 import { motion } from 'framer-motion'
 
-import { List } from '@mui/material'
+import { List, SpeedDial, SpeedDialIcon } from '@mui/material'
 import { MatchItem } from '../../components/MatchItem/MatchItem'
+import { useNavigate } from 'react-router-dom'
 
 export const MatchList = () => {
+  const navigate = useNavigate()
   const loadedMatches = useOwnMatches()
 
   const matchItems = loadedMatches?.map((match: Match) => {
@@ -22,6 +24,20 @@ export const MatchList = () => {
       style={{ width: '100%' }}
     >
       <List sx={{ width: 'inherit' }}>{matchItems}</List>
+      <SpeedDial
+        ariaLabel="Create new match"
+        sx={{
+          '& .MuiButtonBase-root': {
+            position: 'fixed',
+            bottom: '70px',
+            width: '45px',
+            height: '45px',
+          },
+          alignItems: 'end',
+        }}
+        icon={<SpeedDialIcon />}
+        onClick={() => navigate('/create-match')}
+      ></SpeedDial>
     </motion.div>
   )
 }

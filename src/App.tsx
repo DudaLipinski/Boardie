61899b15b3f35a3ec975f84f49d4fd6b8d580f6c
MariaLipinski <dudalipinski@hotmail.com>
2023-03-03 16:43:37 +0100

    feat(createMatch): update to use mui sending just one participant

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material'
 import { theme } from './styles/theme'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 
 function App() {
   return (
     <Provider store={state}>
       <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </LocalizationProvider>
       </ThemeProvider>
     </Provider>
   )
--- src/pages/Match/MatchItem.tsx
+++ src/pages/Match/MatchItem.tsx
@@ -1,147 +1,162 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react'
-import moment, { Moment } from 'moment'
-import { useParams } from 'react-router-dom'
-import { getMatch, createMatch } from '../../services/match'
-import { motion } from 'framer-motion'
-import { useSelector } from 'react-redux'
-import { selectors as userSelectors } from '../../state/user'
-import {
-  Button,
-  Col,
-  Form,
-  Input,
-  Row,
-  DatePicker,
-  Typography,
-  InputNumber,
-} from 'antd'
-import { Match } from '../../types/Match'
-import { Participants } from './Participants'
-import dayjs from 'dayjs'
-import { animationProps } from '../../styles/animation'
+import { useFormik } from 'formik'
 
-const { TextArea } = Input
-const { Title } = Typography
+import { motion } from 'framer-motion'
+
+import dayjs, { Dayjs } from 'dayjs'
+import {
+  Box,
+  Grid,
+  Stack,
+  TextField,
+  Button,
+  FormControlLabel,
+  Checkbox,
+} from '@mui/material'
+import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker'
+import { useCallback } from 'react'
+import React from 'react'
+import { createMatch } from '../../services/match'
 
 export const MatchItem = () => {
-  const { id } = useSelector(userSelectors.getUser)
-  const { matchId } = useParams()
-  const [loading, setLoading] = useState(!!matchId)
-  const [initialValues, setInitialValues] = useState<{
-    boardgameName: string
-    date: Moment
-    duration: number
-    notes: string
-  }>()
+  const formik = useFormik({
+    initialValues: {
+      boardgameName: '',
+      date: dayjs(),
+      duration: 0,
+      notes: '',
+      participants: [
+        {
+          fullName: '',
+          score: 0,
+          isWinner: false,
+        },
+      ],
+    },
+    onSubmit: (values) => {
+      const date = dayjs(values.date).format('ddd, MMMM D, YYYY')
+      const match = { ...values, date: date }
 
-  const loadMatch = async (matchId: string) => {
-    const foundMatch = await getMatch(matchId)
-    setLoading(false)
-    setInitialValues({
-      boardgameName: foundMatch.boardgameName,
-      date: moment(foundMatch.date),
-      duration: foundMatch.duration,
-      notes: foundMatch.notes,
-    })
-  }
+      createMatch(match)
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((error) => alert(error.message))
+    },
+  })
 
-  useEffect(() => {
-    if (matchId) {
-      loadMatch(matchId)
-    }
+  const handleDateChange = useCallback((value: Dayjs | null) => {
+    formik.setFieldValue('date', value)
   }, [])
-
-  if (loading) {
-    return null
-  }
-
-  const handleMatch = (matchData: Match) => {
-    const date = dayjs(matchData.date).format('ddd, MMMM D, YYYY')
-    const match = { ...matchData, authorId: String(id), date: date }
-
-    createMatch(match)
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((error) => alert(error.message))
-  }
+  const renderDateInput = useCallback(
+    (params: any) => <TextField {...params} />,
+    []
+  )
 
   return (
-    <motion.div {...animationProps}>
-      <Row
-        style={{
-          justifyContent: 'center',
-          backgroundColor: 'white',
-          padding: '30px 30px',
-        }}
-      >
-        <Col xs={24} md={24} lg={14} xl={7}>
-          <Title level={2}>Match</Title>
-          <Form
-            layout="vertical"
-            name="create-match"
-            onFinish={handleMatch}
-            autoComplete="off"
-            initialValues={initialValues}
-          >
-            <Form.Item
-              name="boardgameName"
-              label="Boardgame"
-              rules={[
-                {
-                  required: true,
-                  message: `Please input boardgame's name!`,
-                },
-              ]}
-            >
-              <Input placeholder="Boardgame' name" />
-            </Form.Item>
-            <div style={{ display: 'flex', gap: 8 }}>
-              <Form.Item
-                name="date"
+    <motion.div
+      initial={{ opacity: 0 }}
+      transition={{ delay: 0.1 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      style={{ width: '100%' }}
+    >
+      <Box component="form" gap="12px" onSubmit={formik.handleSubmit}>
+        <Stack spacing={2}>
+          <TextField
+            type="text"
+            fullWidth
+            variant="outlined"
+            margin="dense"
+            id="boardgameName"
+            label="Board game"
+            value={formik.values.boardgameName}
+            onChange={formik.handleChange}
+          />
+          <Grid container sx={{ gap: '16px' }}>
+            <Grid item xs={8}>
+              <MobileDateTimePicker
                 label="Date"
-                rules={[
-                  {
-                    required: true,
-                    message: `Please input date!`,
-                  },
-                ]}
-              >
-                <DatePicker />
-              </Form.Item>
-              <Form.Item name="duration" label="Duration">
-                <InputNumber placeholder="Duration" />
-              </Form.Item>
-            </div>
-            <label htmlFor="notes">Notes</label>
-            <TextArea
-              rows={3}
-              name="notes"
-              placeholder="Notes"
-              allowClear={true}
-              style={{ marginBottom: 20 }}
-            />
-            <Form.List
-              initialValue={[{ fullName: '', score: null }]}
-              name="participants"
-            >
-              {(fields, { add, remove }) => (
-                <Participants fields={fields} add={add} remove={remove} />
-              )}
-            </Form.List>
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                style={{ float: 'right' }}
-              >
-                Save
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
+                value={formik.values.date}
+                onChange={handleDateChange}
+                renderInput={renderDateInput}
+              />
+            </Grid>
+            <Grid item xs>
+              <TextField
+                type="number"
+                variant="outlined"
+                id="duration"
+                label="Duration"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={formik.values.duration}
+                onChange={formik.handleChange}
+              />
+            </Grid>
+          </Grid>
+          <TextField
+            multiline
+            variant="outlined"
+            rows={4}
+            id="notes"
+            label="Notes"
+            value={formik.values.notes}
+            onChange={formik.handleChange}
+          />
+          <Grid container sx={{ gap: '16px' }}>
+            {formik.values.participants.map((participant, index) => (
+              <React.Fragment key={index}>
+                <Grid item xs={7}>
+                  <TextField
+                    type="text"
+                    variant="outlined"
+                    margin="dense"
+                    id={`participants[${index}].fullName`}
+                    name={`participants[${index}].fullName`}
+                    label="Participant"
+                    value={participant.fullName}
+                    onChange={formik.handleChange}
+                  />
+                </Grid>
+                <Grid item xs={2} sx={{ marginTop: '8px' }}>
+                  <TextField
+                    type="number"
+                    variant="outlined"
+                    id={`participants[${index}].score`}
+                    name={`participants[${index}].score`}
+                    label="Score"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    value={participant.score}
+                    onChange={formik.handleChange}
+                  />
+                </Grid>
+                <Grid item xs={1}>
+                  <FormControlLabel
+                    color="primary"
+                    sx={{ color: '#7e7e7e', fontSize: '12px', margin: '0' }}
+                    labelPlacement="top"
+                    control={
+                      <Checkbox
+                        checked={formik.values.participants[index].isWinner}
+                      />
+                    }
+                    label="Winner"
+                    name={`participants[${index}].isWinner`}
+                    onChange={formik.handleChange}
+                  />
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+          <Button fullWidth variant="contained" size="large" type="submit">
+            Save
+          </Button>
+        </Stack>
+      </Box>
     </motion.div>
   )
 }

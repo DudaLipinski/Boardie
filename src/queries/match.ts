93234954f380fa7be36f56a48c47acd26e52ce49
Leonardo Diehl <leonardodiehl1@gmail.com>
2023-03-16 20:25:20 +0100

    feat(app/queries): add useOptimisticUpdate hook

--- src/queries/match.ts
+++ src/queries/match.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query'
 import * as matchesService from '../services/match'
 import { Match } from '../types/Match'
+import { useOptimisticUpdate } from '../utils/queries'
 
 const MATCHES_KEY = 'matches'
 export const useMatches = () => {
@@ -23,23 +24,15 @@ export const useMatchCreation = () => {
 
 const DELETE_MATCH_KEY = 'deleteMatch'
 export const useMatchDeletion = () => {
-  const queryClient = useQueryClient()
+  const filterDeletedMatch = useOptimisticUpdate(
+    MATCHES_KEY,
+    (deletedMatchId: number) => (oldMatches: Match[] | undefined) =>
+      oldMatches ? oldMatches.filter(({ id }) => id !== deletedMatchId) : []
+  )
 
-  return useMutation(DELETE_MATCH_KEY, matchesService.deleteMatch, {
-    onMutate: async (deletedMatchId) => {
-      await queryClient.cancelQueries({ queryKey: [MATCHES_KEY] })
-
-      const previousMatches: Match[] | undefined =
-        queryClient.getQueryData(MATCHES_KEY)
-
-      queryClient.setQueryData(MATCHES_KEY, (oldMatches: Match[] | undefined) =>
-        oldMatches ? oldMatches.filter(({ id }) => id !== deletedMatchId) : []
-      )
-
-      return { previousMatches }
-    },
-    onError: (err, deletedMatchId, context) => {
-      queryClient.setQueryData(['todos'], context?.previousMatches)
-    },
-  })
+  return useMutation(
+    DELETE_MATCH_KEY,
+    matchesService.deleteMatch,
+    filterDeletedMatch
+  )
 }

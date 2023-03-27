00fcf84c2d050151138742f372246ec4c10fa515
MariaLipinski <dudalipinski@hotmail.com>
2023-03-27 11:18:41 +0200

    feat(editMatch): update match details, add structure for updating participants

--- src/routes/AuthenticatedRoutes.tsx
+++ src/routes/AuthenticatedRoutes.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
-import { Match } from '../pages/Match/Match'
+import { MatchCreation } from '../pages/MatchCreation/MatchCreation'
 import { Profile } from '../pages/Profile/Profile'
 import { Dashboard } from '../pages/Dashboard/Dashboard'
 import { MatchList } from '../pages/MatchList/MatchList'
+import { MatchEdition } from '../pages/MatchEdition/EditMatch'
 import {
   CREATE_MATCH,
   DASHBOARD,
   MATCHES,
   PROFILE,
   MATCH_DETAILS,
+  EDIT_MATCH,
 } from './routeSpecs'
 
 export const AuthenticatedRoutes = () => {
@@ -16,8 +18,9 @@ export const AuthenticatedRoutes = () => {
     <Routes>
       <Route path={DASHBOARD} element={<Dashboard />} />
       <Route path={MATCHES} element={<MatchList />} />
-      <Route path={MATCH_DETAILS} element={<Match />} />
-      <Route path={CREATE_MATCH} element={<Match />} />
+      <Route path={MATCH_DETAILS} element={<MatchEdition />} />
+      <Route path={CREATE_MATCH} element={<MatchCreation />} />
+      <Route path={EDIT_MATCH} element={<MatchEdition />} />
       <Route path={PROFILE} element={<Profile />} />
       <Route path="*" element={<Dashboard />} />
     </Routes>

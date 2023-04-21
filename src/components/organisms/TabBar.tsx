eb2ada8772b5cd924566871aa5636862450c541f
MariaLipinski <dudalipinski@hotmail.com>
2023-04-21 10:37:42 +0200

    feat(ui): small updates

--- src/components/organisms/TabBar.tsx
+++ src/components/organisms/TabBar.tsx
@@ -19,38 +19,40 @@ export const TabBar = ({ user }: { user: User }) => {
   const genericUser = userToGeneric(user)
 
   return (
-    <nav className="bg-slate-800">
-      <ul className="flex flex-row items-center justify-evenly">
-        <li>
-          <Link to={DASHBOARD} aria-label="Dashboard">
-            <HomeIcon />
-          </Link>
-        </li>
-        <li>
-          <Link to={MATCHES} aria-label="Matches">
-            <CasinoIcon />
-          </Link>
-        </li>
-        <li className="relative -top-4 z-10">
-          <Link
-            to={CREATE_MATCH}
-            aria-label="Create Match"
-            className="flex h-14 w-14 items-center justify-center rounded-full bg-pink-500 text-center "
-          >
-            <AddIcon />
-          </Link>
-        </li>
-        <li>
-          <Link to={FRIENDS} aria-label="Friends">
-            <PeopleIcon />
-          </Link>
-        </li>
-        <li>
-          <Link to={PROFILE} aria-label="Profile">
-            <Avatar user={genericUser} size={'sm'} />
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="bg-slate-800">
+        <ul className="flex flex-row items-center justify-around px-2">
+          <li>
+            <Link to={DASHBOARD} aria-label="Dashboard">
+              <HomeIcon />
+            </Link>
+          </li>
+          <li>
+            <Link to={MATCHES} aria-label="Matches">
+              <CasinoIcon />
+            </Link>
+          </li>
+          <li className="relative -top-5 z-10">
+            <Link
+              to={CREATE_MATCH}
+              aria-label="Create Match"
+              className="flex h-16 w-16 items-center justify-center rounded-full bg-pink-500 text-center "
+            >
+              <AddIcon />
+            </Link>
+          </li>
+          <li>
+            <Link to={FRIENDS} aria-label="Friends">
+              <PeopleIcon />
+            </Link>
+          </li>
+          <li>
+            <Link to={PROFILE} aria-label="Profile">
+              <Avatar user={genericUser} size={'sm'} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
